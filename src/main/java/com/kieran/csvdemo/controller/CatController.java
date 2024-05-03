5d1e162a601224fd4b2540ecc88b8aac382ffc3b
Kieran Mueller (OSV) <kieran.mueller.osv@fedex.com>
2024-05-03 09:52:18 -0500

    added swagger and H2

--- src/main/java/com/kieran/csvdemo/controller/CatController.java
+++ src/main/java/com/kieran/csvdemo/controller/CatController.java
@@ -3,6 +3,7 @@ package com.kieran.csvdemo.controller;
 import com.kieran.csvdemo.service.CatService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,7 +16,7 @@ public class CatController {
 
     private final CatService catService;
 
-    @PostMapping("/upload-cats")
+    @PostMapping(path = "/upload-cats", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<String> uploadCatsFromCsv(@RequestParam("cat_csv") MultipartFile file) {
         log.info("In CatController::uploadCatsFromCsv. Received file with name: {}", file.getOriginalFilename());
         return catService.uploadCatsFromCsv(file);
